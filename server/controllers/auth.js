51383c9652d35137cbb11e28100a34778470c5b6
Ephraim <ephraimigbinosa@gmail.com>
2023-03-13 03:36:31 +0100

    complete authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,3 +38,19 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+/* LOGGING IN  */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials." })
+
+    const token = jwt.sign({ id: user.__id }, process.env.JWT_SECRET);
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
