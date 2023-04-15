ba9afb7786350cc5cbb4640fb786e71f092e7dd2
Ephraim <ephraimigbinosa@gmail.com>
2023-04-15 13:04:09 +0100

    add files from local

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
       picturePath,
       friends,
       location,
-      occupation
+      occupation,
     } = req.body;
 
     const salt = await bcrypt.genSalt();
@@ -29,9 +29,8 @@ export const register = async (req, res) => {
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 10000),
-      impressions: Math.floor(Math.random() * 10000)
+      impressions: Math.floor(Math.random() * 10000),
     });
-
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
@@ -39,20 +38,20 @@ export const register = async (req, res) => {
   }
 };
 
-/* LOGGING IN  */
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: "User does not exist." });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials." })
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user.__id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
-    res.status(500).json({ error: err.message })
+    res.status(500).json({ error: err.message });
   }
-}
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,14 @@
 import express from "express";
-import {getFeedPosts, getUserPost, likePost} from "../controllers/posts.js";
-import {verifyToken} from "../middleware/auth.js";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
-const router = express.Router()
+const router = express.Router();
 
-/* Reading */
+/* READ */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts/", verifyToken, getUserPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
+/* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
